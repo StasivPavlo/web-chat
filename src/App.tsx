ecc85fb4be8d4419bee4a12b93a48066bbff2f83
StasivPavlo <pavlostasiv@gmail.com>
2025-01-15 12:26:43 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-5 w-5 bg-black">
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,26 @@
+import { User } from '../../types/user.ts';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface State {
+  user: User | null
+}
+
+const initialState: State = {
+  user: null,
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<User>) => {
+      state.user =  action.payload;
+    },
+    remove: (state) => {
+      state.user = null;
+    }
+  }
+})
+
+export const { add, remove } = userSlice.actions;
+export default userSlice.reducer;
